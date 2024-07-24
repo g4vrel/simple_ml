c0f55ea3dea1d866cd9a995d66046acb021dd81b
g4vrel <77900910+g4vrel@users.noreply.github.com>
2024-07-24 18:38:20 -0300

    Update utils.py

--- VAE/utils.py
+++ VAE/utils.py
@@ -15,6 +15,6 @@ def get_loaders(config):
     test_data = MNIST(root="data/", train=False, download=True, transform=transform)
     
     train_loader = torch.utils.data.DataLoader(train_data, batch_size=config["bs"], num_workers=config["j"], shuffle=True, drop_last=True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=config["bs"], num_workers=config["j"], shuffle=True, drop_last=True)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=config["bs"], num_workers=config["j"], shuffle=True, drop_last=True) # test
 
     return train_loader, test_loader
